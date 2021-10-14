82bd4f46deba874c3315bb140472ab95bc2cd95e
Siobhán O'Hanlon <Siobhanhanlon@outlook.com>
2021-10-14 14:39:10 +0100

    Add project files.

---
+++ Simple Calculator/Source.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+//Main
+void main()
+{
+	// Declare variables
+	float num1, num2, sum, minus, mult, divide;
+	float amountCal = 0;
+	char oper, cal = 'y';
+
+	//Prompt and input values
+	printf("Please enter number 1\t");
+	scanf("%d", &num1);
+
+	printf("Please enter number 2\t");
+	scanf("%d", &num2);
+
+	//Decide which calculation to do
+	printf("Enter which operator you wish to use: \n a-Addition\ts-Subtraction\tm-Multiplication\td-Division\n");
+	scanf("%c", &oper);
+
+	//Switch Statement for operator chosen
+	switch (oper)
+	{
+		//Addition
+		case 'a':
+		case 'A':
+			sum = (num1 + num2);
+			printf("\n\t%,.2f plus %,.2f is : %,.2f\n", num1,num2,sum);
+		break;
+
+		//Subtraction
+		case 's':
+		case 'S':
+			minus = (num1 - num2);
+			printf("\n\t%,.2f minus %,.2f is : %,.2f\n", num1, num2, minus);
+			break;
+
+		//Multiplication
+		case 'm':
+		case 'M':
+			mult = (num1 * num2);
+			printf("\n\t%,.2f multiplied by %,.2f is : %,.2f\n", num1, num2, mult);
+			break;
+
+		//Division
+		case 'd':
+		case 'D':
+			divide = (num1 / num2);
+			printf("\n\t%,.2f divided by %,.2f is : %,.2f\n", num1, num2, divide);
+			break;
+	}
+}
